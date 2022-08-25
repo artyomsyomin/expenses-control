5435edaec96a9e5e788dc239c1587c5edea1ac27
Artyom <seminartem98@mail.ru>
2022-08-25 20:53:16 +0700

    List items render, filter by year

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -12,6 +12,7 @@ const ExpenseFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by Year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
+          {/* <option value="All">All</option> */}
           <option value="2021">2021</option>
           <option value="2022">2022</option>
           <option value="2023">2023</option>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,44 +1,32 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import ExpenseFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 
 import Card from '../UI/Card';
 
-const Expenses = (props) => {
+const Expenses = ({ items }) => {
   const [filteredYear, setfilteredYear] = useState('2022');
 
   const filterChangeHandler = (selectedYear) => {
     setfilteredYear(selectedYear);
   };
 
+  const filteredExpenses = items.filter(
+    (itemsToFilter) =>
+      itemsToFilter.date.getFullYear().toString() === filteredYear
+  );
+
   return (
-    <Card className="expenses">
-      <ExpenseFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <li>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </li>
   );
 };
 
